4d3b49eceb5d533b12a614e45799e639c05a2053
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-04-08 16:16:10 +0000

    código

--- Apartado3.py
+++ Apartado3.py
@@ -8,7 +8,18 @@ class Calificaciones:
     1er_parcial = list(lista_notas["1er_parcial"])
     2do_parcial = list(lista_notas["2do_parcial"])
     nombre_alumno = list(lista_notas["nombre_alumno"])
-    practicas = list(lista_notas["practicas"])
+    ordinario_practicas = list(lista_notas["ordinario_practicas"])
 
   def aprobado(self):
+    notas = read_csv(self.archivo)
+    lista_notas = []
+    asistencia = list(lista_notas["asistencia"])
+    1er_parcial = list(lista_notas["1er_parcial"])
+    2do_parcial = list(lista_notas["2do_parcial"])
+    nombre_alumno = list(lista_notas["nombre_alumno"])
+    ordinario_practicas = list(lista_notas["ordinario_practicas"])
+    for i in range(len(self.archivo)):
+      if asistencia[i]>= 0.75 and 1er_parcial[i]> 4 and 2d_prcial[i]>4 and ordinario_practicas[i]> 4 and sum(1er_parcial[i*0,3 + 2do_parcial[i]*0,3 + ordinario_practicas[i]*0,4)>5:
+      printe("{}aprobado".format(nombre[i]))
+      
     
