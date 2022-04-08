ad66648dc2ccca76877a8c0c9559da40a679ddb7
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-04-08 15:08:15 +0000

    código

--- Ejercicio.py
+++ Ejercicio.py
@@ -3,4 +3,11 @@ class Calificaciones:
     self.archivo = archivo
   def leerarchivo(self):
     notas = read_csv(self.archivo)
+    lista_notas = []
+    asistencia = list(notas["asistencia"])
+    1er_parcial = list(notas["1er_parcial"])
+    2do_parcial = list(notas["2do_parcial"])
+    nombre_alumno = list(notas["nombre_alumno"])
+    
+    
     
