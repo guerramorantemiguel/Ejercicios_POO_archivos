864526202353d4520c112b768ce6f37ddd8eb26c
miguelguerra <100090620+guerramorantemiguel@users.noreply.github.com>
2022-04-08 15:17:30 +0000

    código

---
+++ Apartado3.py
@@ -0,0 +1,11 @@
+class Calificaciones:
+  def __init__(self, archivo):
+    self.archivo = archivo
+  def leerarchivo(self):
+    notas = read_csv(self.archivo)
+    lista_notas = []
+    asistencia = list(notas["asistencia"])
+    1er_parcial = list(notas["1er_parcial"])
+    2do_parcial = list(notas["2do_parcial"])
+    nombre_alumno = list(notas["nombre_alumno"])
+    practicas = list(notas["practicas"])
